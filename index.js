2a3fa5be37effa6b05e1266b1881da088cf22e60
dungthinhtv <102164139+dungthinhtv@users.noreply.github.com>
2022-10-06 10:21:14 +0700

    1 file modified

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.evn.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   connect();
   console.log('Connected to backend.');
